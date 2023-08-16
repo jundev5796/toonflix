3b4b8ffdd2ccec8f06c17f647d4da6ce3b5fc739
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-16 16:57:22 +0900

    [Webtoon App] Added shadows to image borders

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -57,6 +57,13 @@ class HomeScreen extends StatelessWidget {
               clipBehavior: Clip.hardEdge,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
               ),
               child: Image.network(
                 webtoon.thumb,
