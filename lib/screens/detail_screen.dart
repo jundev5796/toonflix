776f91b7248aa3848f94e928065969d95278cd08
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-18 12:34:41 +0900

    [Webtoon App] Implemented detail genre and age

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -83,9 +83,19 @@ class _DetailScreenState extends State<DetailScreen> {
                     horizontal: 50,
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
                         style: const TextStyle(
                           fontSize: 16,
                         ),
