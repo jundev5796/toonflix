24abf7dbe2fc2e0c4af7760f2a4f7967774fea7a
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-18 12:44:47 +0900

    [Webtoon App] Implemented detail epiosde list

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -107,6 +107,22 @@ class _DetailScreenState extends State<DetailScreen> {
               return const Text("...");
             },
           ),
+          const SizedBox(
+            height: 50,
+          ),
+          FutureBuilder(
+            future: episodes,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    for (var episode in snapshot.data!) Text(episode.title)
+                  ],
+                );
+              }
+              return Container();
+            },
+          ),
         ],
       ),
     );
